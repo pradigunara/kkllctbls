490f5764e93765178319aef457366904b91fea83
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2022-02-20 13:42:05 +0700

    add jisun jiwon, add card variant input

--- pages/input.js
+++ pages/input.js
@@ -1,6 +1,6 @@
-import _ from "lodash";
-import imageCompression from 'browser-image-compression';
-import { useEffect, useState } from "react";
+import _ from 'lodash'
+import imageCompression from 'browser-image-compression'
+import { useEffect, useState } from 'react'
 import { Form, Input, Select, Row, Col, Upload, Button, Switch } from 'antd'
 import db from 'data/db.json'
 
@@ -21,13 +21,27 @@ const submitInput = (data) =>
 export default function InputPage() {
   const [form] = Form.useForm()
   const [selectedEra, setSelectedEra] = useState()
-  const [rounded, setRounded] = useState(true)
+  const [selectedSection, setSelectedSection] = useState()
 
-  const handleFinish = (payload) => {
+  const findVariant = (era, section) => _.find(db.sections?.[era], { code: section })?.variant ?? {}
+
+  const handleFinish = (form) => {
+    const rounded = findVariant(form.eraCode, form.sectionCode)?.[form.name]?.rounded ?? true
+
+    const payload = {
+      ...form,
+      rounded
+    }
+
     submitInput(payload).then(() => alert('OK'))
   }
 
-  const createOptions = (opts = []) => opts.map((o) => ({ label: o.name, value: o.code }))
+  const createOptions = (opts = []) =>
+    opts.map((o) => ({ label: o.name, value: o.code }))
+
+  const nameVariant =
+    _.find(db.sections?.[selectedEra], { code: selectedSection })?.variant ?? {}
+  const nameOptions = _.keys(nameVariant).map((name) => ({ label: name, value: name }))
 
   return (
     <Row justify="center" style={{ marginTop: '5vh' }}>
@@ -53,23 +67,21 @@ export default function InputPage() {
               options={createOptions(db.sections?.[selectedEra])}
               placeholder="Select Section"
               showSearch
+              onSelect={setSelectedSection}
             />
           </Form.Item>
           <Form.Item label="Name" name="name">
-            <Input />
-          </Form.Item>
-          <Form.Item label="Rounded" name="rounded" valuePropName="checked" initialValue={true}>
-            <Switch defaultChecked onChange={() => setRounded(!rounded)}/>
+            <Select
+              options={nameOptions}
+              placeholder="Select Name"
+              showSearch
+            />
           </Form.Item>
           <Form.Item label="Image" name="image">
-            <ImageUploader rounded={rounded}/>
+            <ImageUploader />
           </Form.Item>
           <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              block
-            >
+            <Button type="primary" htmlType="submit" block>
               Submit
             </Button>
           </Form.Item>
@@ -80,60 +92,64 @@ export default function InputPage() {
 }
 
 const FILETYPE_EXT = {
-  'image/jpeg': '.jpg'
-};
-const ALLOWED_FILE_TYPES = _.keys(FILETYPE_EXT);
+  'image/jpeg': '.jpg',
+}
+const ALLOWED_FILE_TYPES = _.keys(FILETYPE_EXT)
 
-const toBase64 = file =>
+const toBase64 = (file) =>
   new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = error => reject(error);
-  });
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = (error) => reject(error)
+  })
 
 function ImageUploader({ value, onChange, filenamePrefix = '', rounded }) {
-  const [internalFileList, setInternalFileList] = useState(value ?? []);
-  const [b64src, setB64src] = useState();
+  const [internalFileList, setInternalFileList] = useState(value ?? [])
+  const [b64src, setB64src] = useState()
 
   const handleChange = async ({ fileList }) => {
-    const listCopy = [...fileList];
-    const lastFile = _.last(listCopy);
+    const listCopy = [...fileList]
+    const lastFile = _.last(listCopy)
 
     if (!_.includes(ALLOWED_FILE_TYPES, lastFile?.type)) {
-      return alert(
-        `File type not allowed (must be jpg)`
-      );
+      return alert(`File type not allowed (must be jpg)`)
     }
 
-    const timestamp = Date.now();
-    const extension = _.get(FILETYPE_EXT, lastFile.type);
-    const name = `${filenamePrefix}-${timestamp}${extension}`;
+    const timestamp = Date.now()
+    const extension = _.get(FILETYPE_EXT, lastFile.type)
+    const name = `${filenamePrefix}-${timestamp}${extension}`
 
     // modify the original file for compression
-    const compressedBlob = await imageCompression(lastFile?.originFileObj ?? lastFile, {
-      maxSizeMB: 4,
-      maxWidthOrHeight: 500
-    });
-    const File = window?.File ? window.File : _.identity();
+    const compressedBlob = await imageCompression(
+      lastFile?.originFileObj ?? lastFile,
+      {
+        maxSizeMB: 4,
+        maxWidthOrHeight: 500,
+      }
+    )
+    const File = window?.File ? window.File : _.identity()
     const compressedFile = new File([compressedBlob], name, {
       lastModified: timestamp,
-      type: compressedBlob.type
-    });
+      type: compressedBlob.type,
+    })
 
-    const base64DataUrl = await toBase64(compressedFile);
-    const base64 = _.last(base64DataUrl?.split('base64,'));
+    const base64DataUrl = await toBase64(compressedFile)
+    const base64 = _.last(base64DataUrl?.split('base64,'))
 
-    const modifiedLastUpload = [{ ...lastFile, originFileObj: compressedFile, base64 }];
+    const modifiedLastUpload = [
+      { ...lastFile, originFileObj: compressedFile, base64 },
+    ]
 
-    setInternalFileList(modifiedLastUpload);
-    setB64src(base64DataUrl);
-    onChange(_.last(modifiedLastUpload)?.base64);
+    setInternalFileList(modifiedLastUpload)
+    setB64src(base64DataUrl)
+    onChange(_.last(modifiedLastUpload)?.base64)
 
-    return true;
+    return true
   }
 
-  const handleDrop = ({ dataTransfer }) => handleChange({ fileList: dataTransfer.files })
+  const handleDrop = ({ dataTransfer }) =>
+    handleChange({ fileList: dataTransfer.files })
 
   return (
     <>
@@ -150,7 +166,14 @@ function ImageUploader({ value, onChange, filenamePrefix = '', rounded }) {
         Drag to Upload Here
       </Upload.Dragger>
       <br />
-      <img src={b64src} style={{ maxHeight: '20vh', borderRadius: rounded && '0.8em' }}/>
+      <img
+        src={b64src}
+        style={{
+          maxHeight: '20vh',
+          borderRadius: rounded && '0.8em',
+          boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)'
+        }}
+      />
     </>
-  );
+  )
 }
