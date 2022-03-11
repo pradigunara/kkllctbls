7b1a869c153a9f2d81d488baf46a7268b6a688dd
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2022-03-11 16:32:25 +0700

    build

---
+++ .next/server/chunks/307.js
@@ -0,0 +1,69 @@
+"use strict";
+exports.id = 307;
+exports.ids = [307];
+exports.modules = {
+
+/***/ 307:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Breadcrumbs)
+/* harmony export */ });
+/* harmony import */ var lodash_castArray__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(9065);
+/* harmony import */ var lodash_castArray__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash_castArray__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var antd__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5725);
+/* harmony import */ var antd__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(antd__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1664);
+/* harmony import */ var _data_db_json__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(5875);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__);
+
+
+
+
+/**
+ * crumbs element is an array of [(text), (link)] tuple
+ *
+ * @param crumbs
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+
+
+function Breadcrumbs({
+  crumbs = []
+}) {
+  return /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(antd__WEBPACK_IMPORTED_MODULE_1__.Row, {
+    children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(antd__WEBPACK_IMPORTED_MODULE_1__.Col, {
+      children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)(antd__WEBPACK_IMPORTED_MODULE_1__.Breadcrumb, {
+        separator: "",
+        children: [/*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(antd__WEBPACK_IMPORTED_MODULE_1__.Breadcrumb.Item, {
+          children: "Page"
+        }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(antd__WEBPACK_IMPORTED_MODULE_1__.Breadcrumb.Separator, {
+          children: ":"
+        }), crumbs.map((crumb, idx) => {
+          const [content, link] = lodash_castArray__WEBPACK_IMPORTED_MODULE_0___default()(crumb);
+
+          return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsxs)("span", {
+            children: [idx > 0 && /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(antd__WEBPACK_IMPORTED_MODULE_1__.Breadcrumb.Separator, {
+              children: '>'
+            }), /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(antd__WEBPACK_IMPORTED_MODULE_1__.Breadcrumb.Item, {
+              children: link ? /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx(next_link__WEBPACK_IMPORTED_MODULE_2__["default"], {
+                href: link,
+                children: /*#__PURE__*/react_jsx_runtime__WEBPACK_IMPORTED_MODULE_4__.jsx("a", {
+                  children: content
+                })
+              }) : content
+            })]
+          }, idx);
+        })]
+      })
+    })
+  });
+}
+
+/***/ })
+
+};
+;
