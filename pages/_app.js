a357e56bf5f865e289d965204eadb45b289ed906
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2022-03-19 06:34:15 +0700

    setup favicons

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import 'antd/dist/antd.css';
 import './index.css'
 import { Row, Col } from 'antd';
-import previewImage from 'public/preview.jpg'
+import previewImage from "../public/preview.jpg";
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -14,16 +14,23 @@ export default function MyApp({ Component, pageProps }) {
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
           key="viewport"
         />
-        <link rel="icon" href="/favicon.png" />
         <meta
           name="description"
           content="gotta kkollect 'em all!"
           key="desc"
         />
         <meta property="og:image" content={previewImage} key="ogimage" />
-        <meta property="og:site_name" content="Kkollectibles" key="ogsitename" />
+        <meta
+          property="og:site_name"
+          content="Kkollectibles"
+          key="ogsitename"
+        />
         <meta property="og:title" content="Kkollectibles" key="ogtitle" />
-        <meta property="og:description" content="gotta kkollect 'em all!" key="ogdesc" />
+        <meta
+          property="og:description"
+          content="gotta kkollect 'em all!"
+          key="ogdesc"
+        />
       </Head>
       <Row
         justify="center"
