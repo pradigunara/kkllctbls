4d86271fa7539b32d6ec5c7cabbd1dc4d58aa90b
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2022-04-10 15:57:24 +0700

    build

---
+++ .next/server/pages/_app.js
@@ -0,0 +1,122 @@
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 3242:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ MyApp)
+});
+
+;// CONCATENATED MODULE: external "next/head"
+const head_namespaceObject = require("next/head");
+var head_default = /*#__PURE__*/__webpack_require__.n(head_namespaceObject);
+// EXTERNAL MODULE: ./node_modules/antd/dist/antd.css
+var antd = __webpack_require__(4722);
+// EXTERNAL MODULE: external "antd"
+var external_antd_ = __webpack_require__(5725);
+;// CONCATENATED MODULE: ./public/preview.jpg
+/* harmony default export */ const preview = ({"src":"/_next/static/media/preview.f5e72017.jpg","height":200,"width":200,"blurDataURL":"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoKCgoKCgsMDAsPEA4QDxYUExMUFiIYGhgaGCIzICUgICUgMy03LCksNy1RQDg4QFFeT0pPXnFlZXGPiI+7u/sBCgoKCgoKCwwMCw8QDhAPFhQTExQWIhgaGBoYIjMgJSAgJSAzLTcsKSw3LVFAODhAUV5PSk9ecWVlcY+Ij7u7+//CABEIAAgACAMBIgACEQEDEQH/xAAoAAEBAAAAAAAAAAAAAAAAAAAABQEBAQAAAAAAAAAAAAAAAAAAAwT/2gAMAwEAAhADEAAAAKAGn//EAB0QAAEEAgMAAAAAAAAAAAAAAAECAwUSABMyUXL/2gAIAQEAAT8AMO2qBESGgH9Fkd35X9E5/8QAGBEAAgMAAAAAAAAAAAAAAAAAAAIBIXH/2gAIAQIBAT8AVpvT/8QAFxEBAAMAAAAAAAAAAAAAAAAAAgAicv/aAAgBAwEBPwBA1zP/2Q=="});
+// EXTERNAL MODULE: external "react/jsx-runtime"
+var jsx_runtime_ = __webpack_require__(997);
+;// CONCATENATED MODULE: ./pages/_app.js
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) { symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); } keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+
+
+
+
+
+
+
+function MyApp({
+  Component,
+  pageProps
+}) {
+  return /*#__PURE__*/(0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
+    children: [/*#__PURE__*/(0,jsx_runtime_.jsxs)((head_default()), {
+      children: [/*#__PURE__*/jsx_runtime_.jsx("title", {
+        children: "Kkollectibles"
+      }), /*#__PURE__*/jsx_runtime_.jsx("meta", {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+      }, "viewport"), /*#__PURE__*/jsx_runtime_.jsx("meta", {
+        name: "description",
+        content: "gotta kkollect 'em all!"
+      }, "desc"), /*#__PURE__*/jsx_runtime_.jsx("meta", {
+        property: "og:image",
+        content: preview
+      }, "ogimage"), /*#__PURE__*/jsx_runtime_.jsx("meta", {
+        property: "og:site_name",
+        content: "Kkollectibles"
+      }, "ogsitename"), /*#__PURE__*/jsx_runtime_.jsx("meta", {
+        property: "og:title",
+        content: "Kkollectibles"
+      }, "ogtitle"), /*#__PURE__*/jsx_runtime_.jsx("meta", {
+        property: "og:description",
+        content: "gotta kkollect 'em all!"
+      }, "ogdesc")]
+    }), /*#__PURE__*/jsx_runtime_.jsx(external_antd_.Row, {
+      justify: "center",
+      style: {
+        fontFamily: 'Architects Daughter, cursive',
+        margin: '1em'
+      },
+      children: /*#__PURE__*/jsx_runtime_.jsx(external_antd_.Col, {
+        xs: 24,
+        md: 10,
+        children: /*#__PURE__*/jsx_runtime_.jsx(Component, _objectSpread({}, pageProps))
+      })
+    })]
+  });
+}
+
+/***/ }),
+
+/***/ 4722:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 5725:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("antd");
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(3242));
+module.exports = __webpack_exports__;
+
+})();
