4e71d43c487ada1a38cda74405f671f60214a1aa
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2023-01-15 18:23:09 +0800

    lazy load main app body

---
+++ components/appbody.jsx
@@ -0,0 +1,28 @@
+import { Row, Col } from 'antd'
+import { getPrimaryColor } from 'data/constants'
+import { ConfigProvider } from 'antd'
+import { useRouter } from 'next/router'
+
+export default function AppBody({ Component, pageProps }) {
+  const router = useRouter()
+  const group = router.query?.group
+
+    return (
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: getPrimaryColor(group),
+            colorLink: getPrimaryColor(group),
+            colorLinkActive: getPrimaryColor(group),
+            colorLinkHover: getPrimaryColor(group),
+            fontFamily: 'Architects Daughter, cursive',
+          },
+        }}
+      >
+          <Row justify="center" style={{ margin: '1em' }}>
+            <Col xs={24} md={10}> <Component {...pageProps} />
+            </Col>
+          </Row>
+      </ConfigProvider>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,11 @@
 import Head from 'next/head'
-import { Row, Col } from 'antd'
-import { getPrimaryColor } from 'data/constants'
-import { ConfigProvider } from 'antd'
-import {useRouter} from 'next/router'
+import dynamic from 'next/dynamic'
 import 'antd/dist/reset.css'
 import './index.css'
 
-export default function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const group = router.query?.group
+const AppBody = dynamic(() => import("components/appbody"), { ssr: false })
 
+export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
@@ -38,22 +34,7 @@ export default function MyApp({ Component, pageProps }) {
         />
       </Head>
 
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: getPrimaryColor(group),
-            colorLink: getPrimaryColor(group),
-            colorLinkActive: getPrimaryColor(group),
-            colorLinkHover: getPrimaryColor(group),
-            fontFamily: 'Architects Daughter, cursive',
-          },
-        }}
-      >
-          <Row justify="center" style={{ margin: '1em' }}>
-            <Col xs={24} md={10}> <Component {...pageProps} />
-            </Col>
-          </Row>
-      </ConfigProvider>
+      <AppBody Component={Component} pageProps={pageProps} />
     </>
   )
 }
